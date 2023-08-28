004c5c7c77ea9603cdcc5a99bec1ad42bb8ec578
Juliana Ifionu <julieifionu@gmail.com>
2023-08-28 14:45:24 -0400

    Set up SASS and tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-green-400 mt-5'>Hello there</div>
+    </div>
+  );
+}
+
+export default App;
